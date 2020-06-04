b3ce50194b4087fac08fe1fb0a7782d44dda8555
Reece Pierson <piersonreece@gmail.com>
2020-06-04 09:39:42 -0500

    increased description, decreases overall request allowance

--- api/data/migrations/20200524130020_migrations.js
+++ api/data/migrations/20200524130020_migrations.js
@@ -5,10 +5,10 @@ exports.up = function(knex) {
         .createTable('projects', (tbl)=>{
             tbl.increments();
             tbl.string('name',255).notNullable();
-            tbl.string('description').notNullable();
+            tbl.string('description', 1000).notNullable();
             tbl.string('technologies', 255).notNullable();
             tbl.string('deployLink',255);
-            tbl.string('githubLink');
+            tbl.string('githubLink',255);
             tbl.string('imgRef', 500000);
         })
   )
